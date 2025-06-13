55a1840c561f5b1a64b29d724b992a442e1d537f
Sidmon101 <64657186+Sidmon101@users.noreply.github.com>
2025-06-13 14:45:06 +0530

    Delete fbvCRUD directory

--- fbvCRUD/fbvApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from fbvApp.models import Student
-# Register your models here.
-admin.site.register(Student)
--- fbvCRUD/fbvApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from fbvApp.models import Student
-
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model=Student
-        fields='__all__'
