5632e7145e56b314a1473b49864ffc699807a61e
Sidmon101 <64657186+Sidmon101@users.noreply.github.com>
2025-06-13 14:34:26 +0530

    Delete templatesdemo directory

--- templatesdemo/templatesapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def renderTemplate(request):
-    myDict={"name":"Sidharth"}
-    return render(request,'templatesApp/firstTemplate.html',context=myDict )
-def renderEmployee(request):
-    myDict={"id":1,"name":"Sidharth","salary":70000}
-    return render(request,'templatesApp/employeeTemplate.html',myDict)
